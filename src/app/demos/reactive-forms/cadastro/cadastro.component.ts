99a05b65d49c714e60db3643fb6016ddbff9dd8a
LuizTkaczyk <luiz8907@hotmail.com>
2021-11-15 14:50:21 -0300

    spa-formulario

--- src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
+import { Usuario } from './../../models/usuario';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  //em caso de erros na declação , alterar para false o stric, no tsconfig.
+  cadastroForm: FormGroup;
+  //importando da classe usuario
+  usuario: Usuario
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    //criando um grupo de formulario com o formBuilder(recomendado) usando arrays
+    this.cadastroForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      senhaConfirmacao: ['']
+    })
+
+  }
+
+  adicionarUsuario() {
+    //capturando o os dados do formulario e guardando em uma variavel
+    //let x = this.cadastroForm.value
+
+
+    //atribuindo os dados do formulario ao usuario
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value)
+
+
   }
 
 }
