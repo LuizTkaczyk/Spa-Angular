7f23df37f46cda9697aa573d8f5d43c3bde7267d
LuizTkaczyk <luiz8907@hotmail.com>
2021-11-15 19:27:33 -0300

    spa-formulario avancado

--- src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,46 +1,92 @@
+import { ValidationMessages, GenericValidator, DisplayMessage } from './generic-form-validation';
 import { Usuario } from './../../models/usuario';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { MASKS, NgBrazilValidators } from 'ng-brazil';
 import { CustomValidators } from 'ng2-validation';
+import { from, fromEvent, merge, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css'],
-  
+
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit, AfterViewInit {
 
-  //em caso de erros na declação , alterar para false o stric, no tsconfig.
-  cadastroForm: FormGroup;
-  //importando da classe usuario
-  usuario: Usuario
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElement: ElementRef[]
 
+
+  cadastroForm: FormGroup;//em caso de erros na declação , alterar para false o stric, no tsconfig.
+  usuario: Usuario //importando da classe usuario
   formResult: string = ''
+  public MASKS = MASKS; //para o uso das mascaras
 
-  public MASKS = MASKS;
 
-  constructor(private formBuilder: FormBuilder) { }
+
+  //VALIDAÇÃO AVANÇADA
+  validationMessage: ValidationMessages
+  genericValidator: GenericValidator
+  displayMessages: DisplayMessage = {}
+
+  constructor(private formBuilder: FormBuilder) {
+    //mensagens retornadas
+    this.validationMessage = {
+      nome: {
+        required: 'O nome é obrigatório',
+        minlength: 'O nome precisa ter no mínimo 2 caracteres',
+        maxlength: 'O nome precisa ter no máximo 50 caracteres'
+      },
+      cpf: {
+        required: 'Informe o CPF',
+        cpf: 'CPF em formato inválido'
+      },
+
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+
+      senha: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      senhaConfirmacao: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    }
+
+    this.genericValidator = new GenericValidator(this.validationMessage)
+  }
+
+
 
   ngOnInit(): void {
 
     //variaveis para comparar se as senhas são iguais
-    let senha  = new FormControl('',[Validators.required, CustomValidators.rangeLength([6,15])]) //A SENHA DEVE TER DE 6 A 15 DIGITOS
-    let senhaConf  = new FormControl('',[Validators.required, CustomValidators.rangeLength([6,15]), CustomValidators.equalTo(senha)])
-
-
-
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]) //A SENHA DEVE TER DE 6 A 15 DIGITOS
+    let senhaConf = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)])
 
     //criando um grupo de formulario com o formBuilder(recomendado) usando arrays
     this.cadastroForm = this.formBuilder.group({
-      nome: ['',[ Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      cpf: ['', [Validators.required,NgBrazilValidators.cpf]],
-      email: ['',[Validators.required, Validators.email]],
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
+      email: ['', [Validators.required, Validators.email]],
       senha: senha,
       senhaConfirmacao: senhaConf
     })
 
+  }
+
+
+
+  ngAfterViewInit(): void {
+    let controlBlurs:Observable<any>[] = this.formInputElement.map((formControl:ElementRef) => fromEvent(formControl.nativeElement, 'blur'))
+    merge(...controlBlurs).subscribe(()=>{
+      this.displayMessages = this.genericValidator.processarMensagens(this.cadastroForm)
+    })
   }
 
   adicionarUsuario() {
@@ -55,7 +101,7 @@ export class CadastroComponent implements OnInit {
       //transformando o resultado em uma string ( o resultado é visto no html!)
       this.formResult = JSON.stringify(this.cadastroForm.value)
 
-    }else{
+    } else {
       this.formResult = "Não submetido"
     }
   }
